ddd1e6c53dabe720b9ae9c4d433dd5bd5c34bf92
Harry Xu <harryxcz@gmail.com>
2023-03-20 22:12:21 -0400

    Add new feed navigation changes

--- RegenquestApp/Routers/Router.js
+++ RegenquestApp/Routers/Router.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import NewPost from '../components/NewPost';
+import Comments from '../components/Comments';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ export default function Router() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="NewPost" component={NewPost} />
+        <Stack.Screen name="Comments" component={Comments} />
       </Stack.Navigator>
     </NavigationContainer>
   );
